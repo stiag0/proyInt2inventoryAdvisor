7b50cf17ed2349694f34ddd4f5b121f24256bbf2
stiag0 <stiago01r@gmail.com>
2020-11-18 01:28:02 -0500

    random forest implementado en el modelo de producto estrella, ahora aplica los 2 moelos predictivos.

--- main/model/prodEstrella.py
+++ main/model/prodEstrella.py
@@ -39,22 +39,17 @@ def metricasD( X_train, X_test, y_train, y_test,clf):
     print('TRAIN: {:.4f}, TEST: {:.4f}'.format(f1_score(y_train_true, y_train_pred.round(),average = 'weighted'), f1_score(y_test_true, y_test_pred.round(),average = 'weighted')))
     return  train, test;
 
-
-def splitByCos(dataset,colY):
+def predictor(dataset,colY):
     idProductos = dataset.idProducto.unique()
     #nombreProducto = dataset.nombreProducto.loc[,]
+    predNextSell = []
     for i in range(0,len(idProductos)):
         idProducto = idProductos[i]        
         df = dataset[dataset['idProducto'] == idProducto]
         df = df.reset_index(drop = True)
-        #conteo = df.index.values[i]
         nombreProducto = df.loc[0,'nombreProducto']
-        
-        #print("nombre %s" % nombreProducto)
-        #df.to_csv('resultados/discriminados/%s.csv' % idProducto)
         cantidadVend = df.cantidadVendida.sum()
         if (cantidadVend > 30):
-            #print ("largo ", nombreProducto)
             arrX = df.idPedido
             arraY = df[colY] 
             X_train, X_test, y_train, y_test = train_test_split(arrX, arraY)
@@ -66,27 +61,26 @@ def splitByCos(dataset,colY):
             #use model to predict on TEST data
             prediction = LR.predict(X_test.values.reshape(-1,1))
             
-            #print(LR.score(X_test.values.reshape(-1,1), y_test.values))
-            #print(LR.score( X_train.values.reshape(-1,1),  y_train.values))
+            train, test = metricasD( X_train, X_test, y_train, y_test,LR)
             
-            metricasD( X_train, X_test, y_train, y_test,LR)
-            
-            
-            #print(LR.score( X_test.values.reshape(-1,1),  X_train.values))
-            #print(LR.score( y_test.values.reshape(-1,1),  y_train.values))
-            
-            plt.figure(figsize=(9,6))
-            #plt.scatter(X_train, y_train, label = 'training Data', color = 'black', alpha = 0.5)
-            #plt.scatter(X_test, y_test, label = 'test Data', color = 'g',alpha = 0.5 )
-            # Error Cuadrado Medio
-            #print("Mean squared error: %.2f" % mean_squared_error(y_train, y_test))
-            # Puntaje de Varianza. El mejor puntaje es un 1.0
-            #print('Variance score: %.2f' % r2_score(y_train, y_test))
-            #plot prediction line against actual test data
-            
-            plt.plot(X_test, prediction, label = 'linear Regresion', color = 'b')
-            plt.scatter(X_test, y_test, label = 'test Data', color = 'g', alpha = 0.6)
-            plt.legend()
-            plt.title("%s "%nombreProducto)
-            
-            plt.show()
+            if(train < 0.85 or test < 0.85 ):
+                print("regresion lineal no suficinete paso a random forest")
+                #try random forest 
+                regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
+                regressor.fit(arrX.values.reshape(-1,1), arraY)
+                dato_pred = regressor.predict([[1602824759775]])[0]
+                print (dato_pred)
+            else:
+                plt.figure(figsize=(9,6))
+
+                dato_pred = LR.predict(np.array([[1602824759775]]))[0]
+                print(dato_pred)
+                
+                plt.plot(X_test, prediction, label = 'linear Regresion', color = 'b')
+                plt.scatter(X_test, y_test, label = 'test Data', color = 'g', alpha = 0.6)
+                plt.legend()
+                plt.title("%s "%nombreProducto) 
+                plt.show()
+                        
+            predNextSell.append(dato_pred)  
+    print(predNextSell)  
